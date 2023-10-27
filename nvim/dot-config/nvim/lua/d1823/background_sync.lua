98d0e3316de01934a2edddd1e7a425476ba5e440
Dawid Wołosowicz <d@1823.pl>
2023-10-27 07:42:19 +0200

    nvim: Make background_sync use dbus-send instead of gsettings
    By querying the `org.freedesktop.appearance.color-scheme` directly,
    we're making sure the configuration in cross-platform as gsettings
    probably works only in the Gnome environment.

--- nvim/dot-config/nvim/lua/d1823/background_sync.lua
+++ nvim/dot-config/nvim/lua/d1823/background_sync.lua
@@ -7,13 +7,18 @@ function M.run()
 
     M.timer = vim.loop.new_timer()
     M.timer:start(M.timer_timeout, M.timer_repeat, vim.schedule_wrap(function()
-        -- Use the GNOME specific org.freedesktop.appearance color-scheme setting to
-        -- figure out the vim background through the system-wide color-scheme
-        -- preference.
+        -- Use the org.freedesktop.appearance.color-scheme setting to figure out
+        -- the vim background.
+        --
+        -- Spec: https://github.com/flatpak/xdg-desktop-portal/blob/main/data/org.freedesktop.impl.portal.Settings.xml#L34
         local bg = vim.fn.system({
             "bash",
             "-c",
-            "gsettings get org.gnome.desktop.interface color-scheme | grep -i prefer-dark > /dev/null && echo dark || echo light"
+            table.concat({
+                "dbus-send --session --dest=org.freedesktop.portal.Desktop --print-reply /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read string:'org.freedesktop.appearance' string:'color-scheme'",
+                "tail -n 1",
+                "awk '{print ($4 == 2) ? \"light\" : \"dark\"}'"
+            }, "|")
         })
         vim.o.background = bg:gsub("%s+", "")
 	vim.cmd [[silent colorscheme solarized8]]
